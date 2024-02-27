61df1dde26a187a29d5ab99dd9aa59265cacd143
Leorobin396 <157003317+Leorobin396@users.noreply.github.com>
2024-02-28 00:43:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
-
-// const dropdowns =document.querySelectorAll(".dropdown select");
-
-// for (let select of dropdowns) {
-//     for (currCode in countryList){
-//         let newOption=document.createElement("option");
-//         newOption.innerText=currCode;
-//         newOption.value=currCode;
-//         if (select.name === "from"&& currCode === "USD"){
-//             newOption.selected="selected";
-//         }else if (select.name === "to"&& currCode === "INR"){
-//             newOption.selected = "selected";
-//         }
-//         select.append(newOption);
-//     }
-
-//     select.addEventListener("change",(evt) =>{
-//         updateFlag(evt,select);
-//     });
-// }
-
-// const updateFlag=(evt,target) =>{
-//     let currCode = target.value;
-//     let countryList = countryList[currCode];
-//     let newSrc = `https://flagsapi.com/${countryList}/flat/64.png`;
-//     let img = element.parentElement.querySelector("img");
-//     img.src = newSrc;
-// };
-
-
 const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
